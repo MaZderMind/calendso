14d597dd5011749473059efbd89bce35d0374753
Peter Körner <pkoerner@seibert-media.net>
2021-05-05 22:37:23 +0200

    specify mixed type explicitly

--- pages/availability/index.tsx
+++ pages/availability/index.tsx
@@ -45,8 +45,8 @@ export default function Availability(props) {
     const closeSuccessModal = () => { router.replace(router.asPath); }
 
     function convertMinsToHrsMins (mins) {
-        let h = Math.floor(mins / 60);
-        let m = mins % 60;
+        let h: string | number = Math.floor(mins / 60);
+        let m: string | number = mins % 60;
         h = h < 10 ? '0' + h : h;
         m = m < 10 ? '0' + m : m;
         return `${h}:${m}`;
